eb36cdb5133a801481837b21ab1c8a0d6d33f498
juanvallejo-wtw <Juan.Vallejo@wtwco.com>
2024-11-07 17:59:37 -0500

    moving log

--- Selenium/tests/GithubTest.cs
+++ Selenium/tests/GithubTest.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using Selenium.core;
+using Selenium.core.browsers;
 using Selenium.pages;
+using System;
 
 namespace Selenium.tests
 {
@@ -13,6 +15,16 @@ namespace Selenium.tests
         {
             var page = new HomePage(Driver);
             page.OpenGithubPage();
+
+
+            // Log relevant configuration details
+            Console.WriteLine("Test Run Configuration:");
+            Console.WriteLine($"Browser: {Config.Browser}");
+            Console.WriteLine($"Platform: {Config.Platform}");
+            Console.WriteLine($"Base URL: {Config.BaseUrl}");
+            Console.WriteLine($"Using Selenium Grid: {Config.UseSeleniumGrid}");
+            Console.WriteLine($"Using Sauce Labs: {Config.UseSauceLabs}");
+            Console.WriteLine($"Using BrowserStack: {Config.UseBrowserstack}");
         }
     }
 }
